d6053bb8f8a22898069388578ed875cca1bbba3c
SegzAyo <jcide22@gmail.com>
2021-03-02 14:19:27 +0000

    selected product template WIP

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from .models import Product
 # Create your views here.
 
 def all_products(request):
-    """ A view to show all shoes, including sorting and search queries """
+    """ A view to show all product, including sorting and search queries """
 
     products = Product.objects.all()
 
@@ -13,3 +13,16 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+
+def selected_product(request, product_id):
+    """ A view to show selected product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/selected_product.html', context)
