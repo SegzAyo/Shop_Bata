add5c481e512b0d6e6f853201b2bf01a24886ea0
SegzAyo <jcide22@gmail.com>
2021-03-04 16:30:32 +0000

    products sorting added

--- products/views.py
+++ products/views.py
@@ -10,20 +10,26 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    Brand = None
 
     if request.GET:
+        if 'brand' in request.GET:
+            Brand = request.GET['brand']
+            products = products.filter(brand=Brand)  
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(descriptions__icontains=query)
+            queries = Q(name__icontains=query) | Q(descriptions__icontains=query) | Q(categories__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
-         'search_term': query,
+        'search_term': query,
+        'current_brand': Brand,
     }
 
     return render(request, 'products/products.html', context)
