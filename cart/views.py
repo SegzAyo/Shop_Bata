ce072f7e25cb6340733addd66d68fe7e435621b2
SegzAyo <jcide22@gmail.com>
2021-03-06 22:53:38 +0000

    cart templates WIP

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def view_cart(request):
-    """ A view that renders the bag contents page """
+    """ A view that renders the content of the shopping cart """
 
     return render(request, 'cart/cart.html')
