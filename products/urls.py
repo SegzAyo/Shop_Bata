9418903c35656f42c8a94a08055c284c189a9fad
SegzAyo <jcide22@gmail.com>
2021-03-18 21:33:55 +0000

    product admin views, url and template added, WIP

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.selected_product, name='selected_product'),
+    path('<int:product_id>/', views.selected_product, name='selected_product'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -65,3 +66,23 @@ def selected_product(request, product_id):
     }
 
     return render(request, 'products/selected_product.html', context)
+
+def add_product(request):
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
