2de6f5eee6600ff499cca6ece77f1dd47a99db78
SegzAyo <jcide22@gmail.com>
2021-03-19 11:36:17 +0000

    product admin update and delete functionality completed

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>/', views.selected_product, name='selected_product'),
+    path('<product_id>/', views.selected_product, name='selected_product'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<product_id>/', views.delete_product, name='delete_product'),
 ]
