8a78934dcf68c08c25989139b4c4089926f19742
SegzAyo <jcide22@gmail.com>
2021-03-25 15:18:57 +0000

    AWS S3 credentials added

--- Shop_Bata/settings.py
+++ Shop_Bata/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     # other
 
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -152,8 +153,6 @@ else:
 
 
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -195,6 +194,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if config('USE_AWS', ''):
+    AWS_STORAGE_BUCKET_NAME = 'segzayo-shop-bata'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    
 
   # Stripe
 FREE_DELIVERY_THRESHOLD = 50
